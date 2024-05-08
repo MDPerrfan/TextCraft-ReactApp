e98013cdb333d13c029b90bac0edea2fee395d62
Mohammed Parves <mdperrfan@gmail.com>
2024-05-08 22:59:11 +0600

    Updated link in Nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar(props) {
     <>
     <nav className="navbar navbar-expand-lg bg-body-tertiary " data-bs-theme={`${props.mode}`}>
     <div className="container-fluid">
-      <a className="navbar-brand" hrer="#">{props.heading}</a>
+      <Link className="navbar-brand" to="/">{props.heading}</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
